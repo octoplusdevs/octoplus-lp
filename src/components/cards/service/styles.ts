54233fd886cf601275b62e11f81aab73de892a99
danguya <daniel.yava16@gmail.com>
2023-02-12 00:31:29 +0100

    creating a service card component

---
+++ src/components/cards/service/styles.ts
@@ -0,0 +1,33 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.div`
+  background-color: #232529;
+  padding: 48px 24px;
+  height: 294px;
+  width: 384px;
+  display: flex;
+  flex-direction: column;
+  gap: 48px;
+  border-radius: 8px;
+  .content {
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+    h3 {
+      font-family: Inter, sans-serif;
+      font-style: normal;
+      font-weight: 600;
+      font-size: 24px;
+      line-height: 140%;
+      color: #ffffff;
+    }
+    p {
+      font-family: Inter, sans-serif;
+      font-style: normal;
+      font-weight: 400;
+      font-size: 16px;
+      line-height: 140%;
+      color: #a6a6a6;
+    }
+  }
+`
